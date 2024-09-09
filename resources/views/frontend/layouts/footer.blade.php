27f974244eb1f25d16ea70f86404c0c42a44ea17
Humdiess <hmzinchannel@gmail.com>
2024-09-09 11:02:07 +0700

    feat: frontend home

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/geist@1.3.1/dist/font.min.js"></script>
+
+</body>
+</html>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/js/app.js')
+    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
+    <title>Document</title>
+
+    <style>
+        body {
+            font-family: 'Geist Sans', sans-serif;
+        }
+    </style>
+</head>
+<body>
+
+
