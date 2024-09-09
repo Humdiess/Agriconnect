de11ce40364323552c3b0c0430ae0d4043704424
Humdiess <hmzinchannel@gmail.com>
2024-09-09 20:44:32 +0700

    feat: install font

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1,3 +1,13 @@
 @include('frontend.layouts.header')
 
+<section class="hero-container container mx-auto">
+    <div class="hero-wrapper w-100">
+        <div class="hero w-100 flex justify-center">
+            <div class="hero-title max-w-md mt-24">
+                <h1 class="hero-title-text text-center text-3xl"><span class="bg-gradient-to-r from-[#27a002] to-[#1d1515] bg-clip-text text-transparent">Agri</span>Connect: Membangun Petani Maju dengan AI, IoT, dan Pemasaran Digital</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
 @include('frontend.layouts.footer')
--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -8,12 +8,16 @@
     <!-- Meta Description -->
     <meta name="description" content="Agriculture Connect is an online platform for farmers">
 
+    <link href="https://cdn.jsdelivr.net/npm/@fontsource/geist-sans@5.0.3/600.min.css" rel="stylesheet">
+
     @vite('resources/css/app.css')
     <style>
-
+        body {
+            font-family: 'Geist Sans', sans-serif;
+        }
     </style>
 </head>
-<body>
+<body class="antialiased">
 
     @include('frontend.components.navbar')
 
