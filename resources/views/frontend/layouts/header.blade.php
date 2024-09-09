d575f79163ce1a25288b3485bb225a52b7e4b509
Humdiess <hmzinchannel@gmail.com>
2024-09-10 00:03:00 +0700

    docs: add plus jakarta sans font

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -8,6 +8,12 @@
     <!-- Meta Description -->
     <meta name="description" content="Agriculture Connect is an online platform for farmers">
 
+    {{-- font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+
+    {{-- css --}}
     @vite('resources/css/app.css')
     <style>
         body {
