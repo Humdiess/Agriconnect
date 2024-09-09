8947fe726b65701e1e170a1d5a8799b7addd0387
Humdiess <hmzinchannel@gmail.com>
2024-09-09 20:31:50 +0700

    docs: update meta tag

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Viewport for responsive web design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <title>AgriConnect</title>
+    <!-- Meta Description -->
+    <meta name="description" content="Agriculture Connect is an online platform for farmers">
 
     @vite('resources/css/app.css')
     <style>
