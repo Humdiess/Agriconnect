64713343cdc91ae4df1fd445d1ed036ddc2caf15
Humdiess <hmzinchannel@gmail.com>
2024-10-09 14:30:39 +0700

    fix (frotnend) : footer kurang rame

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -1,5 +1,15 @@
 <x-user-footer />
 
+<script src="{{ asset('js/navbar.js') }}"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="https://kit.fontawesome.com/cff8b87f33.js" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/lenis@1.1.13/dist/lenis.min.js"></script>
+@include('frontend.components.gsap')
+<script src="{{ asset('js/script.js') }}"></script>
+<script src="{{ asset('js/gsap.js') }}"></script>
+<script src="{{ asset('js/preload.js') }}"></script>
+@include('sweetalert::alert')
+
 <script>
     document.querySelectorAll('.accordion-toggle').forEach((button) => {
         button.addEventListener('click', function() {
@@ -7,32 +17,6 @@
             accordion.classList.toggle('active');
         });
     });
-</script>
-
-{{-- Navbar functionality --}}
-<script src="{{ asset('js/navbar.js') }}"></script>
-
-{{-- AOS (Animate On Scroll) library --}}
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
-{{-- FontAwesome for icons --}}
-<script src="https://kit.fontawesome.com/cff8b87f33.js" crossorigin="anonymous"></script>
-
-{{-- Lenis smooth scroll --}}
-<script src="https://unpkg.com/lenis@1.1.13/dist/lenis.min.js"></script>
-
-{{-- GSAP (GreenSock Animation Platform) --}}
-@include('frontend.components.gsap')
-
-<script src="{{ asset('js/script.js') }}"></script>
-<script src="{{ asset('js/gsap.js') }}"></script>
-<script src="{{ asset('js/preload.js') }}"></script>
-
-{{-- SweetAlert --}}
-@include('sweetalert::alert')
-
-<script>
-    let mouseX = 0, mouseY = 0, posX = 0, posY = 0;
 
     gsap.to(".cursor-example", {
         duration: 0.018,
